d2b7370cf286b530453690a66ea748c196756f44
Solo <juan@xmartlabs.com>
2023-12-29 15:27:31 -0300

    adds shared constants

---
+++ lib/constants.ts
@@ -0,0 +1,32 @@
+export enum Routes {
+  Root = "/",
+  SignIn = "/sign-in",
+}
+
+export const headerLinks = [
+  {
+    label: "Home",
+    route: "/",
+  },
+  {
+    label: "Create Event",
+    route: "/events/create",
+  },
+  {
+    label: "My Profile",
+    route: "/profile",
+  },
+];
+
+export const eventDefaultValues = {
+  title: "",
+  description: "",
+  location: "",
+  imageUrl: "",
+  startDateTime: new Date(),
+  endDateTime: new Date(),
+  categoryId: "",
+  price: "",
+  isFree: false,
+  url: "",
+};
