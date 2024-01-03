14839afe38c57cca51a8ea0678ca6ec917f1aee9
Solo <juan@xmartlabs.com>
2024-01-03 19:06:51 -0300

    form, input and label base components

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -17,9 +17,9 @@ export async function POST(req: Request) {
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
-    throw new Error(
-      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
-    );
+    return new Response("Error occured -- missing webhook secret", {
+      status: 500,
+    });
   }
 
   // Get the headers
