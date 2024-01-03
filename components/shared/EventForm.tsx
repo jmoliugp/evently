6b5a0135a4d3ceb94f38914ac2a488625347e39f
Solo <juan@xmartlabs.com>
2024-01-03 19:07:33 -0300

    event form shared component

---
+++ components/shared/EventForm.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import * as z from "zod";
+import { eventFormSchema } from "@/lib/validator";
+import { eventDefaultValues } from "@/lib/constants";
+
+interface Props {
+  userId: string;
+  type: "Create" | "Update";
+}
+
+const EventForm: React.FC<Props> = ({ type, userId }) => {
+  const initialValues = eventDefaultValues;
+
+  const form = useForm<z.infer<typeof eventFormSchema>>({
+    resolver: zodResolver(eventFormSchema),
+    defaultValues: initialValues,
+  });
+
+  function onSubmit(values: z.infer<typeof eventFormSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input
+                    placeholder="Event title"
+                    {...field}
+                    className="input-field"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button
+          type="submit"
+          size="lg"
+          disabled={form.formState.isSubmitting}
+          className="button col-span-2 w-full"
+        >
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default EventForm;
