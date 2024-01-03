c06d38a396ca8bf0cbbc4b71c208b97713defb4d
Solo <juan@xmartlabs.com>
2024-01-03 20:02:37 -0300

    add category alert dialog within categories form event

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -8,6 +8,18 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { CategoryDocument } from "@/lib/database/models/category.model";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Input } from "@/components/ui/input";
 
 interface Props {
   value?: string;
@@ -15,10 +27,13 @@ interface Props {
 }
 
 export const Dropdown: React.FC<Props> = ({ onChangeHandler, value }) => {
-  const [categories, setCategories] = useState<CategoryDocument[]>([
-    { _id: 1, name: "Category 1" },
-    { _id: 2, name: "Category 2" },
-  ]);
+  const [categories, setCategories] = useState<CategoryDocument[]>([]);
+  const [category, setCategory] = useState("");
+
+  // TODO: Implement.
+  const addCategory = () => {
+    console.log("🚀 ~ addCategory onAction");
+  };
 
   return (
     <Select onValueChange={onChangeHandler} defaultValue={value}>
@@ -26,9 +41,6 @@ export const Dropdown: React.FC<Props> = ({ onChangeHandler, value }) => {
         <SelectValue placeholder="Category" />
       </SelectTrigger>
       <SelectContent>
-        {/* <SelectItem value="light">Light</SelectItem>
-        <SelectItem value="dark">Dark</SelectItem>
-        <SelectItem value="system">System</SelectItem> */}
         {categories.length > 0 &&
           categories.map((category) => {
             const categoryId = category._id.toString();
@@ -39,6 +51,26 @@ export const Dropdown: React.FC<Props> = ({ onChangeHandler, value }) => {
               </SelectItem>
             );
           })}
+        <AlertDialog>
+          <AlertDialogTrigger className="p-medium-14 flex w-full rounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500">
+            Open
+          </AlertDialogTrigger>
+          <AlertDialogContent className="bg-white">
+            <AlertDialogHeader>
+              <AlertDialogTitle>New Category</AlertDialogTitle>
+              <AlertDialogDescription></AlertDialogDescription>
+              <Input
+                type="text"
+                placeholder="Category name"
+                className="input-field mt-3"
+              />
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction onClick={addCategory}>Add</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </SelectContent>
     </Select>
   );
