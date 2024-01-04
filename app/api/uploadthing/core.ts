17b29908ceabba1814ad3da1f7d503e0f9d0c129
Solo <juan@xmartlabs.com>
2024-01-04 20:43:40 -0300

    Uploadthing dropzone helper

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,28 @@
+// Ref: https://docs.uploadthing.com/api-reference/react#uploaddropzone
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
+
+export const ourFileRouter = {
+  imageUploader: f({ image: { maxFileSize: "4MB" } })
+    .middleware(async ({ req }) => {
+      const user = await auth(req);
+
+      if (!user) throw new Error("Unauthorized");
+
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      // This code RUNS ON YOUR SERVER after upload
+      console.log("Upload complete for userId:", metadata.userId);
+
+      console.log("file url", file.url);
+
+      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      return { uploadedBy: metadata.userId };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
