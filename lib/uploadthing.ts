e3333657123528ee92547a837c2a24930fb96e5a
Solo <juan@xmartlabs.com>
2024-01-12 17:31:07 -0300

    uploadthing helper

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -1,5 +1,6 @@
-import { OurFileRouter } from "@/app/api/uploadthing/core";
-import { generateComponents } from "@uploadthing/react";
+import { generateReactHelpers } from "@uploadthing/react/hooks";
 
-export const { UploadButton, UploadDropzone, Uploader } =
-  generateComponents<OurFileRouter>();
+import type { OurFileRouter } from "@/app/api/uploadthing/core";
+
+export const { useUploadThing, uploadFiles } =
+  generateReactHelpers<OurFileRouter>();
