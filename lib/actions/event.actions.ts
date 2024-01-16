fcc7e4a48adc27ddbe628b7a0f80746faf7b6f47
Solo <juan@xmartlabs.com>
2024-01-16 17:34:50 -0300

    event action to get event with details

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -2,7 +2,8 @@
 
 import { prisma } from "@/lib/database";
 import { handleError } from "@/lib/utils/handleError";
-import { CreateEventParams } from "@/types";
+import { CreateEventParams, Event } from "@/types";
+import { Event as DbEvent } from "@prisma/client";
 
 export const createEvent = async ({
   event,
@@ -33,5 +34,48 @@ export const createEvent = async ({
     return newEvent;
   } catch (error) {
     handleError(error);
+  }
+};
+
+export const getEvent = async (id: string): Promise<DbEvent> => {
+  let event;
+  try {
+    event = await prisma.event.findFirst({ where: { id } });
+  } catch (error) {
+    handleError(error);
+  }
+
+  if (!event) throw new Error("Event not found");
+
+  return event;
+};
+
+export const getEventWithDetails = async (eventId: string): Promise<Event> => {
+  try {
+    const eventWithDetails = await prisma.event.findUnique({
+      where: { id: eventId },
+      include: {
+        organizer: {
+          select: {
+            id: true,
+            firstName: true,
+            lastName: true,
+          },
+        },
+        category: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+      },
+    });
+
+    if (eventWithDetails == null) throw new Error("Event not found");
+
+    return eventWithDetails;
+  } catch (error) {
+    handleError(error);
+    throw new Error("There was an error fetching the event");
   }
 };
