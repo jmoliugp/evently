6e07c3359f479b6d86035abf48d1c1673cb74bb6
Solo <juan@xmartlabs.com>
2024-01-26 18:14:34 -0300

    check out button integration on event details

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { CheckoutButton } from "@/components/shared/CheckoutButton";
 import { Collection } from "@/components/shared/Collection";
 import {
   getEventWithDetails,
@@ -55,6 +56,8 @@ const EventDetails: React.FC<SearchParamProps> = async ({
             </div>
 
             {/* CHECKOUT BUTTON */}
+            <CheckoutButton event={event} />
+
             <div className="flex flex-col gap-5">
               <div className="flex gap-2 md:gap-3">
                 <Image
