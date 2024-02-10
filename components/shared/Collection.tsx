bb08b1d7c47e2b57b491bef0ccb06bec9c6c5dce
Solo <juan@xmartlabs.com>
2024-02-10 15:28:09 -0300

    collection fix on one element scenario

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -26,14 +26,14 @@ export const Collection: React.FC<Props> = ({
   type,
   urlParamName,
 }) => {
-  if (data.length <= 1)
+  if (data.length < 1) {
     return (
       <div className="flex-center wrapper min-h-[200px] w-full flex-col gap-3 rounded-[14px] bg-grey-50 py-28 text-center">
         <h3 className="p-bold-20 md:h5-bold">{emptyTitle}</h3>
         <p className="p-regular-14">{emptyStateSubtext}</p>
       </div>
     );
-
+  }
   const hasOrderLink = type === "EventsOrganized";
   const hidePrice = type === "MyTickets";
 
